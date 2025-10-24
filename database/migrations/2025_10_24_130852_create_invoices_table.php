420e68ea9285c3301acedc00af0ac0c638701d88
Javad Mohamadi <javadmohamadi.dev@gmail.com>
2025-10-24 19:05:58 +0330

    add transactions and invoices migrations

---
+++ database/migrations/2025_10_24_130852_create_invoices_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\InvoiceStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 20, 2);
+            $table->enum('status', InvoiceStatus::cases())->default(InvoiceStatus::PENDING);
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamp('paid_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
