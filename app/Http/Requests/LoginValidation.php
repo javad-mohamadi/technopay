db9c578476331978a3765776f11832fe125651f7
Javad Mohamadi <javadmohamadi.dev@gmail.com>
2025-10-25 12:37:42 +0330

    fix validation name

--- app/Http/Requests/LoginValidation.php
+++ app/Http/Requests/LoginValidation.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RegisterRequest extends FormRequest
+class LoginValidation extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -19,11 +19,9 @@ class RegisterRequest extends FormRequest
      */
     public function rules(): array
     {
-
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:4'],
+            'email' => 'required|exists:users,email',
+            'password' => 'required|min:4|max:30',
         ];
     }
 }
